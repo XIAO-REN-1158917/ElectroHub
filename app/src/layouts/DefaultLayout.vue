8ebe83bab7acac7dea37a886892517f0e5e561b4
Gary Ren <espresso1014@gmail.com>
2025-07-10 09:59:38 +1200

    Update DefaultLayout.vue
    feat: implement basic layout with aside, header and main section.

--- app/src/layouts/DefaultLayout.vue
+++ app/src/layouts/DefaultLayout.vue
@@ -1,3 +1,24 @@
 <template>
-    I am layout
+    <el-container>
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
 </template>
+
+<style lang="less" scoped>
+.el-aside{
+    height: 100vh;
+    background-color: white;
+    box-shadow: 10px 0 8px -2px rgba(0, 0, 0, 0.2);
+}
+.el-header{
+    padding: 0 !important;
+}
+.el-main{
+    height: 80vh;
+    overflow: auto;
+}
+</style>
