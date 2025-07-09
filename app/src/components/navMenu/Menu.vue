3df2d4afaf0dea1c354d8b8e576c3171425357c0
Gary Ren <espresso1014@gmail.com>
2025-07-10 11:18:19 +1200

    feat: implement recursive role-based aside menu
    Used a recursive component to render the aside menu dynamically based on user role and menu structure.

---
+++ app/src/components/navMenu/Menu.vue
@@ -0,0 +1,16 @@
+<template>
+    <el-menu>
+        <!-- To maintain consistency with the Element Plus component sytle -->
+        <!-- <menu-item> is used here, but <MenuItem> would also work. -->
+        <menu-item v-for="item in menuItems" :item="item" :key="item.url"></menu-item>
+    </el-menu>
+</template>
+
+<script setup lang="ts">
+import { useUserStore } from '@/store/auth';
+import MenuItem from './MenuItem.vue';
+
+const userStore = useUserStore();
+const menuItems = userStore.menu;
+console.log(userStore.menu);
+</script>
--- app/src/layouts/DefaultLayout.vue
+++ app/src/layouts/DefaultLayout.vue
@@ -1,12 +1,19 @@
 <template>
     <el-container>
-      <el-aside width="200px">Aside</el-aside>
+      <el-aside width="200px">
+        <Menu></Menu>
+      </el-aside>
       <el-container>
         <el-header>Header</el-header>
         <el-main>Main</el-main>
       </el-container>
     </el-container>
 </template>
+
+<script setup>
+import Menu from '@/components/navMenu/Menu.vue';
+
+</script>
 
 <style lang="less" scoped>
 .el-aside{
