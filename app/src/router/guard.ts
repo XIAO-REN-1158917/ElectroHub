31ef9185baba4aa627b9b869b2d2f950558dac5f
Gary Ren <espresso1014@gmail.com>
2025-07-10 15:14:18 +1200

    feat: set up route guard and page access control

--- app/src/router/guard.ts
+++ app/src/router/guard.ts
@@ -13,5 +13,14 @@ router.beforeEach((to) => {
         if (to.path === "/login") {
             return { path: "/" }
         }
+        //First, check if the route has a meta property. 
+        // If it does, and the required permissions do not include the user's existing permissions, 
+        // deny access and redirect to the homepage.
+        if (to.meta?.needAuth && !userStore.roles.some((role: string) => (to.meta.needAuth as string[]).includes(role))) {
+            return { path: "/" }
+        }
     }
 })
+
+// It's worth noting that, in most cases, the backend still verifies the token,
+// so even if the roles property is manually modified in local, unauthorised access is still not possible.
