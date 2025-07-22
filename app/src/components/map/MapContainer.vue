6f13395d09a19985c6781aed2ff14930e7ea0bb2
Gary Ren <espresso1014@gmail.com>
2025-07-23 10:58:35 +1200

    feat: integrate Google Maps API and display map in MapContainer.vue
    - Applied for and configured Google Maps API key with proper restrictions
    - Set up environment variable for secure API key usage
    - Dynamically loaded Maps JavaScript API in MapContainer.vue
    - Successfully rendered interactive map using Google Maps

---
+++ app/src/components/map/MapContainer.vue
@@ -0,0 +1,43 @@
+<template>
+    <div ref="mapRef" id="map" style="width: 100%; height: 400px;"></div>
+</template>
+
+<script lang="ts" setup>
+console.log(import.meta.env.VITE_TEST_VALUE)
+import { onMounted,ref } from 'vue'
+
+const mapRef = ref<HTMLElement | null>(null)
+
+onMounted(async () => {
+await loadGoogleMapsApi()
+
+const { Map } = await (window as any).google.maps.importLibrary('maps')
+
+if (!mapRef.value) {
+    console.error('Map container not found!')
+    return
+  }
+new Map(mapRef.value, {
+    center: { lat: -36.8485, lng: 174.7633 },
+    zoom: 13,
+  })
+})
+
+function loadGoogleMapsApi(): Promise<void> {
+  return new Promise((resolve, reject) => {
+    if ((window as any).google?.maps?.importLibrary) {
+      resolve()
+      return
+    }
+
+    const script = document.createElement('script')
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}&v=weekly`
+
+    script.async = true
+    script.defer = true
+    script.onload = () => resolve()
+    script.onerror = () => reject(new Error('Google Maps failed to load'))
+    document.head.appendChild(script)
+  })
+}
+</script>
--- app/src/views/map/ElectornicMap.vue
+++ app/src/views/map/ElectornicMap.vue
@@ -1,3 +1,11 @@
 <template>
-    Electronic Map view
+    <div>
+    <h2>Map Wrapper</h2>
+    <MapContainer />
+  </div>
+
 </template>
+
+<script lang="ts" setup>
+import MapContainer from '@/components/map/MapContainer.vue';
+</script>
