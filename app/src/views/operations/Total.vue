2e0141f67b1432599163a9766ff87419940f32b4
Gary Ren <espresso1014@gmail.com>
2025-08-21 09:53:33 +1200

    feat(tariff config): implement fuzzy search for city list

--- app/src/views/operations/Total.vue
+++ app/src/views/operations/Total.vue
@@ -2,16 +2,21 @@
     <el-row>
         <el-col :span="6">
             <el-card>
-                <el-input style="width:80%;" placeholder="Enter Key Words" v-model="filterText">
+                <el-input 
+                style="width:80%;" 
+                placeholder="Enter Key Words" 
+                v-model="filterText">
                     <template #append>
                         <el-button icon="Search"></el-button>
                     </template>
                 </el-input>
                 <el-tree 
+                ref="treeRef"
                 class="mt"
                 style="max-width: 600px;" 
                 :props="defaultProps"
                 :data="cityListData"
+                :filter-node-method="filterNode"
                 ></el-tree>
             </el-card>
         </el-col>
@@ -20,8 +25,9 @@
 </template>
 
 <script lang="ts" setup>
-import { onMounted, ref } from "vue"
+import { onMounted, ref, watch } from "vue"
 import { cityListApi } from "@/api/operations"
+import type {ElTree} from "element-plus"
 
 interface CityListDataType {
     label: string,
@@ -30,6 +36,11 @@ interface CityListDataType {
 
 const filterText = ref<string>("")
 const cityListData = ref<CityListDataType[]>([])
+const treeRef = ref<InstanceType<typeof ElTree>>()
+
+watch(filterText, (val) => {
+    treeRef.value!.filter(val)
+})
 
 const defaultProps = {
     children: "children",
@@ -44,4 +55,9 @@ onMounted(async() => {
         console.log(error)
     }
 })
+
+const filterNode = (value:string,data:CityListDataType) => {
+    if (!value) return true
+    return data.label.includes(value)
+}
 </script>
