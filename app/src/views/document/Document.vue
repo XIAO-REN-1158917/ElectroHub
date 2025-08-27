a66b3c2526142e0c76d294629ae78ebbfa670f3d
Gary Ren <espresso1014@gmail.com>
2025-08-28 11:33:10 +1200

    feat: implement linkage between three single-select tag groups and selected tags
    -Added logic to manage three tag groups (single-select each)
    
    -Synced selection state with the “selected” list
    
    -Ensured updates handle new selections, changes, and reset to default

--- app/src/views/document/Document.vue
+++ app/src/views/document/Document.vue
@@ -1,22 +1,53 @@
 <template>
     <el-card>
+        <!-- Tag-Category -->
         <div class="mt">
             <span class="title">Category</span>
-            <el-tag type="info" class="mr" v-for="item in articleTag.category">{{ item }}</el-tag>
+            <el-tag :type="currentTagIndex[0]===-1?'primary':'info'" class="ml mr" @click="handleSelectTag(-1,0,'')">Default</el-tag>
+            <el-tag 
+            :type="currentTagIndex[0]===index?'primary':'info'" 
+            class="mr" 
+            v-for="(item,index) in articleTag.category"
+            @click="handleSelectTag(index,0,item)"
+            >{{ item }}</el-tag>
         </div>
+
+        <!-- Tag-Priority -->
         <div class="mt">
             <span class="title">Priority</span>
-            <el-tag type="info" class="mr" v-for="item in articleTag.priority">{{ item }}</el-tag>
+            <el-tag :type="currentTagIndex[1]===-1?'primary':'info'" class="ml mr" @click="handleSelectTag(-1,1,'')">Default</el-tag>
+            <el-tag 
+            :type="currentTagIndex[1]===index?'primary':'info'" 
+            class="mr" 
+            v-for="(item,index) in articleTag.priority"
+            @click="handleSelectTag(index,1,item)"
+            >{{ item }}</el-tag>
         </div>
+
+        <!-- Tag-Distribution -->
         <div class="mt">
             <span class="title">Distribution</span>
-            <el-tag type="primary" class="ml mr">All</el-tag>
-            <el-tag type="info" class="mr" v-for="item in articleTag.distribution">{{ item }}</el-tag>
+            <el-tag :type="currentTagIndex[2]===-1?'primary':'info'" class="ml mr" @click="handleSelectTag(-1,2,'')">Default</el-tag>
+            <el-tag 
+            :type="currentTagIndex[2]===index?'primary':'info'" 
+            class="mr" 
+            v-for="(item,index) in articleTag.distribution"
+            @click="handleSelectTag(index,2,item)"
+            >{{ item }}</el-tag>
         </div>
         <el-divider/>
+
+        <!-- Selected Tag -->
         <div class="mt">
             <span class="title">Selected</span>
-            <el-tag type="success" class="mr" closable>Example</el-tag>
+            <el-tag 
+            type="success" 
+            class="mr" 
+            closable
+            v-for="item in selectedTagArray"
+            :key="item.group"
+            @close="handleClose(item.group)"
+            >{{ item.tag }}</el-tag>
         </div>
     </el-card>
 </template>
@@ -30,6 +61,12 @@ interface ArticleTagType{
     priority: string[],
     distribution: string[]
 }
+
+interface SelectedTagType{
+    tag: string,
+    group:number
+}
+
 const articleTag = ref<ArticleTagType>({
     category: [],
     priority: [],
@@ -44,6 +81,33 @@ onMounted(async() => {
         console.log(error)
     }
 })
+
+const currentTagIndex = ref([-1, -1, -1])
+const selectedTagArray=ref<SelectedTagType[]>([])
+
+const handleSelectTag = (tagIndex: number, group: number, tag: string) => {
+    currentTagIndex.value[group] = tagIndex
+
+    const existedIndex = selectedTagArray.value.findIndex((item: SelectedTagType) => item.group === group)
+
+    if (!tag) {// select Default
+        selectedTagArray.value = selectedTagArray.value.filter((item: SelectedTagType) => item.group != group)
+    } else {
+        if (existedIndex === -1) {
+            selectedTagArray.value.push({ tag, group })
+        } else {
+            selectedTagArray.value[existedIndex] = { tag, group }
+        }
+    }
+
+}
+const handleClose=(group:number)=>{
+    selectedTagArray.value=selectedTagArray.value.filter((item:SelectedTagType)=>item.group!=group);
+    currentTagIndex.value[group]=-1
+}
+
+
+
 </script>
 
 <style lang="less" scoped>
@@ -51,5 +115,8 @@ onMounted(async() => {
     display: inline-block;
     width: 80px;
     font-size: 14px;
+}
+.el-tag{
+    cursor: pointer;
 }
 </style>
